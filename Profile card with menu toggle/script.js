2263d6ed3e8388776cb4ef1a9734b3d99dbacf2e
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-01-06 10:35:55 +0500

    Update script.js

--- Profile card with menu toggle/script.js
+++ Profile card with menu toggle/script.js
@@ -1,2 +1,6 @@
 const moreBtn = document.querySelector(".more-btn");
 const moreMenu = document.querySelector(".more-menu");
+
+moreBtn.addEventListener("click", () => {
+  moreMenu.classList.toggle("show");
+});
