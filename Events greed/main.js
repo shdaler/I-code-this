8834f214bd8794ffa12e19c5f4722f0c724805df
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-01-13 09:59:12 +0500

    Update main.js

--- Events greed/main.js
+++ Events greed/main.js
@@ -3,3 +3,7 @@ const dotsEl = document.getElementById("dots");
 const themeSelect = document.getElementById("theme");
 const cardEl = document.getElementById("card");
 const themeSelect = document.getElementById("joined");
+
+dotsEl.addEventListener("click", () => {
+  themeSelect.classList.toggle("show");
+});
