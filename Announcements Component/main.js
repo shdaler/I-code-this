c1e756360a56665840be817c654fbd894c594885
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-01-29 08:55:55 +0500

    Update main.js

--- Announcements Component/main.js
+++ Announcements Component/main.js
@@ -1,2 +1,4 @@
 const announcementNumEl = document.getElementById("announcement-num");
 const announcementItemEl = document.querySelectorAll("#announcement-item");
+const dismissBtnEl = document.getElementById("dismiss-all-btn");
+const resetBtnEl = document.getElementById("reset-btn");
