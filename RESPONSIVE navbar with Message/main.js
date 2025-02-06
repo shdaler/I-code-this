34a55a56ebb5bb852aa692200e72961e4108bb2a
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-06 16:07:47 +0500

    Update main.js

--- RESPONSIVE navbar with Message/main.js
+++ RESPONSIVE navbar with Message/main.js
@@ -1,3 +1,18 @@
 const mobileMenuButton = document.getElementById("mobile-menu-btn");
 const closeButton = document.getElementById("close-btn");
 const navList = document.getElementById("nav-list");
+
+const messageContainer = document.getElementById("message-container");
+const closeMessageBtn = document.getElementById("close-message");
+
+closeMessageBtn.addEventListener("click", () => {
+  messageContainer.style.display = "none";
+});
+
+mobileMenuButton.addEventListener("click", () => {
+  navList.classList.add("active");
+});
+
+closeButton.addEventListener("click", () => {
+  navList.classList.remove("active");
+});
