9e76ad9a37fddbfd07650733e96c628ce2ebd666
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-15 11:28:30 +0500

    Update main.js

---
+++ Subscribe Popup/main.js
@@ -0,0 +1,12 @@
+let mainForm = document.getElementById("main-form");
+let form = document.getElementById("form");
+let emailInput = document.getElementById("emailInput");
+let check = document.getElementById("custom-check");
+let submitBtn = document.getElementById("main-btn");
+let formSubmitted = document.getElementById("form-submitted");
+
+let returnBtn = document.getElementById("return");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
