526df82e06796579944fd0705253b72603a9f606
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-04-15 10:16:55 +0500

    Update main.js

---
+++ A Signin and Signup Form With Fade/main.js
@@ -0,0 +1,3 @@
+const tabBtnContainer = document.getElementById("tab-btn-container");
+const tabBtns = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
