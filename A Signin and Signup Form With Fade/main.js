b026b87e321ef38eb94fa9dbcde0ebbc521d73b1
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-04-15 12:09:13 +0500

    Update main.js

--- A Signin and Signup Form With Fade/main.js
+++ A Signin and Signup Form With Fade/main.js
@@ -1,3 +1,24 @@
 const tabBtnContainer = document.getElementById("tab-btn-container");
 const tabBtns = document.querySelectorAll(".tab-btn");
 const tabContents = document.querySelectorAll(".tab-content");
+
+tabBtnContainer.addEventListener("click", (e) => {
+  const selectedBtn = e.target;
+
+  if (selectedBtn.classList.contains("tab-btn")) {
+    const actionName = selectedBtn.getAttribute("data-subject");
+
+    tabContents.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    tabBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    document.getElementById(actionName).style.display = "block";
+    selectedBtn.classList.add("active");
+  }
+});
+
+document.querySelector(".tab-btn").click();
