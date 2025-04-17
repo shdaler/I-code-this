a14eb1f1468b3f59c59594aa0d0fb23ec93188c9
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-04-17 10:09:29 +0500

    Update main.js

--- A Modal window/main.js
+++ A Modal window/main.js
@@ -1,3 +1,9 @@
 const modalButton = document.getElementById("show-modal");
 const mainModalContainer = document.getElementById("modal-container");
 const closeMainModalButton = document.getElementById("close-modal-button");
+
+const modalOk = document.getElementById("modal-ok");
+const modalCancel = document.getElementById("modal-cancel");
+
+const modalAcceptContainer = document.getElementById("modal-accept-container");
+const modalAcceptContainer = document.getElementById("modal-accept-container");
